cb410d2964db4fa4fdff0f17f4999f1cff43ee91
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2022-12-27 21:12:35 +0900

    デバッグでシーン切り替えできるように

---
+++ Assets/MH/Scripts/Debug/SceneChange.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+namespace MH.DebugSystems
+{
+    /// <summary>
+    /// シーンを切り替えるだけのデバッグクラス
+    /// </summary>
+    public sealed class SceneChange : MonoBehaviour
+    {
+        [SerializeField]
+        private string sceneName;
+        
+        private void Start()
+        {
+            SceneManager.LoadScene(this.sceneName);
+        }
+    }
+}
