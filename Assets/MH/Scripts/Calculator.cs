1ed349d16dc246e6b1b70a2cfb06f49bd5b79427
gif-hara <5396546+gif-hara@users.noreply.github.com>
2023-02-02 17:12:57 +0900

    Update Calculator.cs

--- Assets/MH/Scripts/Calculator.cs
+++ Assets/MH/Scripts/Calculator.cs
@@ -3,12 +3,20 @@ using UnityEngine;
 namespace MH
 {
     /// <summary>
+    /// ゲームに必要な各種計算を行うクラス
     /// </summary>
     public static class Calculator
     {
-        public static int GetDamageData(int weaponStrength, int motionPower, float partRate)
+        public static int GetDamageData(int weaponStrength, int motionPower, float partRate, int criticalRate)
         {
-            return Mathf.FloorToInt((weaponStrength * motionPower) * partRate);
+            var damage = (weaponStrength * motionPower) * partRate;
+            var isCritical = (criticalRate * 0.01f) > Random.value;
+            if(isCritical)
+            {
+                damage *= 1.2f;
+            }
+            
+            return Mathf.FloorToInt(damage);
         }
     }
 }
